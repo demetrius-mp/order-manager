d5c00a840189b3dcca05e5d3814771d9d5dd5e02
Demetrius <demetrius.mp789@gmail.com>
2021-08-21 17:55:00 -0400

    feat: foreign key pragma on sqlite database

--- app/entities/database.py
+++ app/entities/database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+from sqlite3 import Connection as SQLite3Connection
+
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -10,3 +12,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+# noinspection PyUnusedLocal
+def _fk_pragma_on_connect(dbapi_con, connection_record):
+    if isinstance(dbapi_con, SQLite3Connection):
+        dbapi_con.execute('pragma foreign_keys=ON')
+
+
+event.listen(engine, 'connect', _fk_pragma_on_connect)
