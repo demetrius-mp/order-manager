a1a402a9799d8832e323f55792755bfd5613e880
Demetrius <demetrius.mp789@gmail.com>
2021-08-21 20:36:41 -0400

    feat: adding cross origin support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import items, orders, order_items
 from app.entities.database import engine, Base
@@ -7,6 +8,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(orders.router)
 app.include_router(items.router)
