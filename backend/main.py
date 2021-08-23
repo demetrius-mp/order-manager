8d0324df8d69dcc456fc75c356ace9e1ab5726f0
Demetrius <demetrius.mp789@gmail.com>
2021-08-22 23:44:48 -0400

    refactor: main app now is on /main.py

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,15 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter
 
 from backend.routers import items, orders, order_items
 from backend.entities.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+router = APIRouter(
+    prefix="",
+    responses={404: {"description": "Not found"}},
 )
 
-app.include_router(orders.router, prefix='/api/v1')
-app.include_router(items.router, prefix='/api/v1')
-app.include_router(order_items.router, prefix='/api/v1')
+router.include_router(orders.router, prefix='/api/v1')
+router.include_router(items.router, prefix='/api/v1')
+router.include_router(order_items.router, prefix='/api/v1')
