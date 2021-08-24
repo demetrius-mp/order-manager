84efe0088f7d6a2a14be50506b608c410a721bd2
Demetrius <demetrius.mp789@gmail.com>
2021-08-24 17:48:16 -0400

    refactor: disabling local function warning and renaming variables

--- app/backend/entities/database.py
+++ app/backend/entities/database.py
@@ -6,15 +6,16 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 
+# noinspection PyUnusedLocal
 def _fk_pragma_on_connect(dbapi_con, con_record):
     dbapi_con.execute('pragma foreign_keys=ON')
 
 
-def get_configured_engine(SQLALCHEMY_DATABASE_URL):
+def get_configured_engine(database_url):
     if SQLALCHEMY_DATABASE_URL.startswith('sqlite'):
-        engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-        event.listen(engine, 'connect', _fk_pragma_on_connect)
-        return engine
+        engine_ = create_engine(database_url, connect_args={"check_same_thread": False})
+        event.listen(engine_, 'connect', _fk_pragma_on_connect)
+        return engine_
 
     return create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -23,13 +24,6 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = get_configured_engine(SQLALCHEMY_DATABASE_URL)
-
-# if SQLALCHEMY_DATABASE_URL.startswith('sqlite'):
-#     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-#     event.listen(engine, 'connect', _fk_pragma_on_connect)
-
-# else:
-#     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
