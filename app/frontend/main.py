d631b2592d1e47804ee1bc3985755c004c67d37f
Demetrius <demetrius.mp789@gmail.com>
2021-08-25 12:01:44 -0400

    refactor: admin panel with orders route

--- app/frontend/main.py
+++ app/frontend/main.py
@@ -4,21 +4,12 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
-router.mount("/static", StaticFiles(directory="app/frontend/static"), name="static")
+router.mount(
+    "/static", StaticFiles(directory="app/frontend/static"), name="static")
 templates = Jinja2Templates(directory="app/frontend/templates")
 
 
 @router.get('/')
-@router.get('/home')
-def home(request: Request):
-    return templates.TemplateResponse("home.html", {'request': request})
-
-
 @router.get('/pedidos')
 def pedidos(request: Request):
     return templates.TemplateResponse("pedidos.html", {'request': request})
-
-
-@router.get('/admin')
-def admin(request: Request):
-    return templates.TemplateResponse("admin.html", {'request': request})
